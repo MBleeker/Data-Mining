5b48a072e55eb3657695ed4989a9e1b31da836f9
liam <liam.sch@gmail.com>
2016-05-02 11:22:09 +0200

    work from first meeting

--- project-two/data_load_and_process.py
+++ project-two/data_load_and_process.py
@@ -12,28 +12,59 @@ class DataContainer:
     def __init__(self, train_data = None, test_data = None, train_size = 50, 
                  test_size = 50):
         self.train_data = train_data
-        self.train_data = test_data
-        self.train_size = train_size
-        self.test_size = test_size
+        self.test_data = test_data
         self.train_drop_cols = ['srch_id','date_time','click_bool',
                                 'booking_bool','gross_bookings_usd']
-    def split_train_data(self):
-        self.train_data_train_subset = \
-            self.train_data[self.train_data['srch_id'] < self.train_size]
-        self.train_data_test_subset = \
-            self.train_data[np.bitwise_and(self.train_data['srch_id']>=self.train_size, 
-                                self.train_data['srch_id']<(self.train_size+self.test_size))]
-    def get_train_Xy(self, data):
-        data = self.preprocess_training_data(data)
+        self.preprocessed = False
+        
+    def get_Xy(self, data):
+        if not self.preprocessed:
+            data = self.preprocess(data)
         X = data.drop(self.drop_cols, axis=1).values
         y = np.maximum(data['click_bool'].values, 
                        data['booking_bool'].values * 5)
         return X, y
-    def preprocess_training_data(self, data):
+    def preprocess(self, data):
         data = data.fillna(data.mean())
+        self.
+        self.preprocessed = True
         return data
-
+        
 data_cont = DataContainer(train_data = train_data)
-data_cont.split_train_data()
-X_train, y_train = data_cont.get_train_Xy(data_cont.train_data_train_subset)
-X_test, y_test = data_cont.get_train_Xy(data_cont.train_data_test_subset)
+X, y = data_cont.get_Xy()
+
+#%%
+v = 'comp1_rate_percent_diff'
+print (train_data[v])
+print sum(train_data[v]==0)*1./len(train_data)
+print sum(np.isnan(train_data[v].values))*1./len(train_data)
+#%%
+import matplotlib.pyplot as plt
+cols = ['visitor_hist_starrating','visitor_hist_adr_usd','prop_starrating',
+        'prop_review_score','prop_location_score2',
+        'prop_log_historical_price','srch_query_affinity_score','comp1_rate',
+        'comp1_inv','comp1_rate_percent_diff']
+nans = np.isnan(train_data[cols])
+nans = pd.DataFrame(nans)
+corr = nans.corr()
+size = corr.shape[0]
+fig, ax = plt.subplots(figsize=(size, size))
+cax = ax.matshow(corr)
+fig.colorbar(cax, ticks=[-1, 0, 1])
+plt.xticks(range(len(corr.columns)), corr.columns);
+plt.yticks(range(len(corr.columns)), corr.columns);
+
+plt.show()
+
+#%%(27-train_data.drop(train_data.columns[27:], axis=1).count(axis=1)).unique()
+#%%
+
+
+#==============================================================================
+#     def split_train_data(self):
+#         self.train_data_train_subset = \
+#             self.train_data[self.train_data['srch_id'] < self.train_size]
+#         self.train_data_test_subset = \
+#             self.train_data[np.bitwise_and(self.train_data['srch_id']>=self.train_size, 
+#                                 self.train_data['srch_id']<(self.train_size+self.test_size))]
+#==============================================================================
